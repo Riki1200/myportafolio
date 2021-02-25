2f294f3337bc9cc37a5d472f415285cedc9fd882
Romeo <45272844+Riki1200@users.noreply.github.com>
2021-02-25 01:39:05 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,21 +15,20 @@ export default () => {
         <p className='text_info--me'>
           
           Hi, <mark> my name is Romeo</mark>, i'm a Developer, i am from Dominican Republic, i am grateful that you look at my portfolio.
-          In this portafolio i'll share my knowlegde and my projects done 
-          to measure i get knowledge. Actually i'm student of end
+          In this portafolio i'll share my knowlegde and my projects done. I am currently a final student
         </p>
       </div>
     <div className='about_div'>
       <h2> <mark>What i do?</mark>  </h2> 
       <p>
-        I dedicate to the investigate about programming or new knowledge realated to the science,technologies(computer and more) and more 
+          I am dedicated to research on programming or new knowledge related to science, technologies (computer science and more) and more.
       </p>
     </div>
 
   <div className='about_div'>
       <h2> <mark> What i want to do?</mark> </h2> 
       <p>
-        I want demostring my knowledge and skills. Always am dispose to learning new skills(knowledgde)
+        I'll want demostring my knowledge and skills. I am always willing to continue learning(knowledgde)
       </p>
     </div>
   
